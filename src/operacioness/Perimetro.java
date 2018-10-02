247751f0bbaf129be141f1e935bcffd1a46109d4
ndreu <cadiaz49@misena.edu.co>
2018-10-02 08:38:07 -0500

    Perimetro

--- src/operacioness/Perimetro.java
+++ src/operacioness/Perimetro.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package operacioness;
+import java.util.Scanner;
 
 /**
  *
  * @author SOPORTE
  */
 public class Perimetro {
+    
+    public static void main(String[] args) {
+        
+        Scanner ingresar = new Scanner (System.in);
+        
+        int Ingreso;
+        
+        System.out.println("Ingrese medida de un lado del cuadrado");
+        Ingreso = ingresar.nextInt();
+        
+        System.out.printf("El perimetro de %d es : "+Ingreso*4, Ingreso);
+        
+        
+    }
     
 }
